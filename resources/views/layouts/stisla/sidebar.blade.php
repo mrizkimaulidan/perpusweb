22151f30cbdc54a0b557fc069dc0d986ea767156
mrizkimaulidan <mrizkimaulidan21@gmail.com>
2020-11-07 14:53:54 +0800

    remove stisla docs on sidebar

--- resources/views/layouts/stisla/sidebar.blade.php
+++ resources/views/layouts/stisla/sidebar.blade.php
@@ -63,10 +63,5 @@
       @endif
     </ul>
 
-    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Documentation
-      </a>
-    </div>
   </aside>
 </div>
