0b9238e8f6cf477f574e1e7f732f4473c3fdf9be
mrizkimaulidan <mrizkimaulidan21@gmail.com>
2020-11-07 14:46:13 +0800

    blade autoformat

--- resources/views/layouts/stisla/sidebar.blade.php
+++ resources/views/layouts/stisla/sidebar.blade.php
@@ -9,19 +9,23 @@
     </div>
     <ul class="sidebar-menu">
       <li class="{{ Request::segment(2) === 'dashboard' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('admin.dashboard.index') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+        <a class="nav-link" href="{{ route('admin.dashboard.index') }}"><i class="fas fa-fire"></i>
+          <span>Dashboard</span></a>
       </li>
       <li class="menu-header">Data Master</li>
       <li class="{{ Request::segment(2) === 'users' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('admin.users.index') }}"><i class="far fa-user"></i> <span>Pengguna</span></a>
+        <a class="nav-link" href="{{ route('admin.users.index') }}"><i class="far fa-user"></i>
+          <span>Pengguna</span></a>
       </li>
       <li class="{{ Request::segment(2) === 'book-types' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('admin.book-types.index') }}"><i class="far fa-bookmark"></i> <span>Kategori Buku</span></a>
+        <a class="nav-link" href="{{ route('admin.book-types.index') }}"><i class="far fa-bookmark"></i> <span>Kategori
+            Buku</span></a>
       </li>
       <li class="{{ Request::segment(2) === 'books' ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('admin.books.index') }}"><i class="fas fa-book"></i> <span>Buku</span></a>
       </li>
-      <li class="nav-item dropdown {{ (Request::segment(2) === 'book-borrowers' ? 'active' : '') || Request::segment(2) === 'book-borrowers-history' ? 'active' : '' }}">
+      <li
+        class="nav-item dropdown {{ (Request::segment(2) === 'book-borrowers' ? 'active' : '') || Request::segment(2) === 'book-borrowers-history' ? 'active' : '' }}">
         <a href="#" class="nav-link has-dropdown"><i class="fas fa-book-reader"></i> <span>Peminjaman</span></a>
         <ul class="dropdown-menu">
           <li class="{{ Request::segment(2) === 'book-borrowers' ? 'active' : '' }}">
@@ -41,11 +45,13 @@
       </div>
       <ul class="sidebar-menu">
         <li class="{{ Request::segment(2) === 'dashboard' ? 'active' : '' }}">
-          <a class="nav-link" href="{{ route('anggota.dashboard.index') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+          <a class="nav-link" href="{{ route('anggota.dashboard.index') }}"><i class="fas fa-fire"></i>
+            <span>Dashboard</span></a>
         </li>
 
         <li class="menu-header">Menu</li>
-        <li class="nav-item dropdown {{ (Request::segment(2) === 'book-borrowers' ? 'active' : '') || Request::segment(2) === 'book-borrowers-history' ? 'active' : '' }}">
+        <li
+          class="nav-item dropdown {{ (Request::segment(2) === 'book-borrowers' ? 'active' : '') || Request::segment(2) === 'book-borrowers-history' ? 'active' : '' }}">
           <a href="#" class="nav-link has-dropdown"><i class="fas fa-book-reader"></i> <span>Peminjaman</span></a>
           <ul class="dropdown-menu">
             <li class="{{ Request::segment(2) === 'book-borrowers-history' ? 'active' : '' }}">
